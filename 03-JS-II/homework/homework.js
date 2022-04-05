6f0f76921ec84766945acd1876f629788f2ca104
nilohuergo <nilohuergo@gmail.com>
2022-04-05 19:19:05 -0300

    JS 2 Homework/done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -165,32 +165,62 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+  if (numero<2) {
+    return false;
+  }
+  if (numero===0) {
+    return true
+  }
+  for (var i = 2; i < numero; i++) {if (numero%i===0) {return false;
+
+  }}
+  return true }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero”
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor===true) {
+    return 'Soy verdadero'
+  }
+  else {
+    return 'Soy falso'
+  }
 
 }
 
-function tablaDelSeis(){
+function tablaDelSeis(numerou){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-
-}
+  let arrayTablaDel6 = []
+ for (let i = 0; i <11; i++) {
+   arrayTablaDel6.push(6*i)
+ } return arrayTablaDel6
+ }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+if (99<numero&&numero<1000) {
+  return true;
+} else {
+  return false
 
+}
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
-  //Usar el bucle do ... while.
+  //Usar el bucle do ... while
+  var a = numero;
+  var i = 0;
+  do {
+    i=i+1;
+    a=a+5;
+  } while (i<8);
+  return a;
 }
 
 
